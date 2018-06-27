70c9d7bd14903bf088d0af125b454d07409291f3
Ankur Nigam <ankur.nigam198@gmail.com>
2018-06-27 16:49:28 +0530

    Static and dynamic binding raspi

---
+++ Phase 1/HomeManager2-DynamicBinding/LightManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace HomeManager
+{
+    public class LightManager
+    {
+        public ObservableCollection<Light> Lights = new ObservableCollection<Light>();
+
+        public bool AddLight(string Nickname, int pin)
+        {
+            Light light = new Light(Nickname, 100, false, pin, TransitionSpeed);
+
+
+            if (Lights.ToList().Find(x => x.Nickname == light.Nickname || x.Pin == pin) != null)
+            {
+                Debug.WriteLine("Duplicate name or pin");
+                return false;
+            }
+            else
+            {
+                Lights.Add(light);
+                GpioManager.MakeOutput(light.Pin);
+                return true;
+            }
+        }
+
+        public Light this[int index]
+        {
+            get { return Lights[0]; }
+            private set { /* set the specified index to value here */ }
+        }
+
+        public int TransitionSpeed = 50;
+
+    }
+
+}
---
+++ Phase 1/HomeManager2-DynamicBinding/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HomeManager
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        LightManager HallLights = new LightManager();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            HallLights.AddLight("Tubelight", 13);
+
+        }
+
+        private void btnAddLight_Click(object sender, RoutedEventArgs e)
+        {
+            HallLights.AddLight(txtNickname.Text, int.Parse(txtPin.Text));
+        }
+    }
+}
